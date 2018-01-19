b5148fea02bd515d424d93c622495a619bb29af8
Guanglei Hu <huguangleiapply@gmail.com>
2018-01-18 20:57:56 -0400

    runnable

--- src/main/java/com/guanglei/HttpRequest/EventDemo.java
+++ src/main/java/com/guanglei/HttpRequest/EventDemo.java
@@ -1,23 +1,43 @@
 package com.guanglei.HttpRequest;
-
+/*
 public class EventDemo extends Thread {
-		EventDemo(){
-			
+		int mounth = 0;
+		EventDemo(int num){
+			mounth = num;
 		}
 		HalifaxEvent event=new HalifaxEvent();
 		
 		public void run(){
-			event.getFebruaryEvent();
-			event.getJanuaryEvent();
+			event.getEventByMounth(mounth);
 		}
 		
 		public static void main(String[] args) {
-			EventDemo h1=new EventDemo();
-			EventDemo h2=new EventDemo();
+			EventDemo h1=new EventDemo(1);
+			EventDemo h2=new EventDemo(2);
 	        h1.start();
 	        h2.start();
 	    }
 
 		
+}
+*/
+
+public class EventDemo implements Runnable {
+    int mounth;
+    public EventDemo(int x) {
+        mounth = x;
+    }
+    HalifaxEvent event=new HalifaxEvent();
+    public void run() {
+    	event.getEventByMounth(mounth);
+    }
+    static public void main(String args[]) {
+    	 Thread t1 = new Thread (new EventDemo(1));
+         t1.start();
+         Thread t2 = new Thread (new EventDemo(2));
+         t2.start();
+   
+    }
+
 }
 
--- src/main/java/com/guanglei/HttpRequest/HalifaxEvent.java
+++ src/main/java/com/guanglei/HttpRequest/HalifaxEvent.java
@@ -23,89 +23,48 @@ import org.slf4j.LoggerFactory;
 
 public class HalifaxEvent {
 	
-	public void getFebruaryEvent() {
+	public void getEventByMounth(int num) {
 		Logger logger = LoggerFactory.getLogger( HalifaxEvent.class );
-		String url = "http://www.destinationhalifax.com/content/winter-events-halifax";
-		try {
-			Document doc = Jsoup.connect(url).get();
-			Element content = doc.getElementById("content");
-			Elements event = content.select("span[style*=color: rgb(51, 51, 51);]");
-			System.out.println("The February has following events: ");
-			for (Element a : event) {
-				  String eventText = a.text();
-				  if(eventText.contains("February"))
-					  System.out.println(eventText);		  
-			}
-		}
-		catch(Exception e) {
-			logger.error("Something is wrong here");
-		}
-		
-		
-		//create a socket
-		/*
-		String hostname = "www.destinationhalifax.com";
-		String path="/content/winter-events-halifax";
-		int port = 80;
-		try {
-			Socket socket = new Socket(InetAddress.getByName(hostname), port);
-			BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(
-			socket.getOutputStream(), "UTF8"));
-			wr.write("GET "+ path +" HTTP/1.1\r\n");
-			wr.write("HOST:" + "www.destinationhalifax.com" + "\r\n");
-			wr.write("\r\n");
-			wr.flush(); 
-			// receive returned value
-			BufferedReader rd = new BufferedReader(new InputStreamReader(
-			socket.getInputStream()));
-			
-			String line;
-			System.out.println("The February has following events: ");
-			while ((line = rd.readLine()) != null) {
-				if(line.contains("February")&&!line.startsWith("<p>"))
-				{
-					line =StringUtils.substringBetween(line,"blank", "</a>");
-					line =StringUtils.remove(line,'"');
-					line =StringUtils.remove(line,"&nbsp");
-					line =StringUtils.remove(line,"<em>");
-					line =StringUtils.remove(line,"</em>");
-					line =StringUtils.remove(line,";");
-					line =StringUtils.remove(line,"amp");
-					line =StringUtils.remove(line,">");
-					System.out.println(line);
+		switch(num){
+			case 1:
+			{	
+				String url = "http://www.destinationhalifax.com/content/winter-events-halifax";
+				try {
+					Document doc = Jsoup.connect(url).get();
+					Element content = doc.getElementById("content");
+					Elements event = content.select("span[style*=color: rgb(51, 51, 51);]");
+					System.out.println("The February has following events: ");
+					for (Element a : event) {
+						  String eventText = a.text();
+						  if(eventText.contains("February"))
+							  System.out.println(eventText);		  
+					}
 				}
+				catch(Exception e) {
+					logger.error("Something is wrong here" , e);
+				}
+				break;
 			}
-			wr.close();
-			rd.close();
-			socket.close();
-			
-			
+			case 2:
+			{
+				String url = "http://www.destinationhalifax.com/content/winter-events-halifax";
+				try {
+					Document doc = Jsoup.connect(url).get();
+					Element content = doc.getElementById("content");
+					Elements links = content.select("span[style*=font-size:16px;]");
+					Elements event = links.select("a[target*=_blank]");
+					System.out.println("The January has following events: ");
+					for (Element a : event) {
+						  String eventText = a.text();
+						  System.out.println(eventText);		  
+					}  
+				}
+				catch(Exception e) {
+					logger.error("Something is wrong here");
+				}
+				break;
+			}	
 		}
-		catch(Exception e){
-			logger.error("Something is wrong here");
-		}
-		*/
-		
-	}
-	
-	public void getJanuaryEvent() {
-		Logger logger = LoggerFactory.getLogger( HalifaxEvent.class );
-		String url = "http://www.destinationhalifax.com/content/winter-events-halifax";
-		try {
-			Document doc = Jsoup.connect(url).get();
-			Element content = doc.getElementById("content");
-			Elements links = content.select("span[style*=font-size:16px;]");
-			Elements event = links.select("a[target*=_blank]");
-			System.out.println("The January has following events: ");
-			for (Element a : event) {
-				  String eventText = a.text();
-				  System.out.println(eventText);		  
-			}  
-		}
-		catch(Exception e) {
-			logger.error("Something is wrong here");
-		}
-		
 	}
 }
 		
